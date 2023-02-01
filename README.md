ffd43387972aeee8ce36153251f06bc469874a3c
Shubham <shubhampark007@gmail.com>
2023-02-01 21:17:59 +0530

    readme

--- README.md
+++ README.md
@@ -12,3 +12,43 @@ Employers can:
 - Manage the hiring process through the website
 
 The goal of a job portal is to make the job search and hiring process more efficient and convenient for both job seekers and employers.
+
+## Installation Instructions
+### Prerequisites
+- Python
+- Flask
+- A web browser
+
+### For Linux/Mac
+1. Install venv:
+sudo apt-get install python3.6-venv
+
+2. Create a virtual environment:
+python3 -m venv env
+
+3. Activate the virtual environment:
+source env/bin/activate
+
+### For Windows
+1. Install venv:
+py -m pip install --user virtualenv
+
+
+2. Create a virtual environment:
+py -m venv env
+
+
+3. Activate the virtual environment:
+.\env\Scripts\activate
+
+
+4. Clone the repository:
+git clone https://github.com/Shubhamprakash007/Job_portal.git
+
+5. Install dependencies:
+pip install -r requirements.txt
+
+6. Run the website:
+python run.py
+
+Access the website by visiting `http://localhost:5000` in your web browser.
--- job_portal/forms.py
+++ job_portal/forms.py
@@ -13,12 +13,10 @@ class StudentRegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    
     def validate_username(self, username):
         user = Student.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
-
     def validate_email(self, email):
         user = Student.query.filter_by(email=email.data).first()
         if user:
@@ -35,17 +33,14 @@ class EmployerRegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
 def validate_username(self, username):
     employer = Employer.query.filter_by(username=username.data).first()
     if employer:
         raise ValidationError('That username is taken. Please choose a different one.')
-
 def validate_email(self, email):
     employer = Employer.query.filter_by(email=email.data).first()
     if employer:
         raise ValidationError('That email is taken. Please choose a different one.')
-
 
 
 class StudentLoginForm(FlaskForm):
@@ -54,6 +49,7 @@ class StudentLoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
 
 class EmployerLoginForm(FlaskForm):
     email = StringField('Email',
@@ -73,6 +69,7 @@ class ResumeForm(FlaskForm):
     user_id = IntegerField('Student ID', validators=[DataRequired()])
     job_id = IntegerField('Job ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 class JobPostForm(FlaskForm):
     job_title = StringField('Job Title', validators=[DataRequired()])
